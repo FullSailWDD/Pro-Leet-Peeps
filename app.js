22a32b1d54e41c0ce26a7465094cfea6b5ee0ef8
Jeffrey Mendez <jeffmendez19@gmail.com>
2015-10-09 14:34:46 -0400

    adding revised file structure

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/app'));
 app.use(express.static(__dirname + '/bower_components'));
 
 //config port
