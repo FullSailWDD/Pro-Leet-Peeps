97e4d75194c21d9576b25f1c4a5fa76061389a89
Jimin Hong <jhong.dev@gmail.com>
2015-10-13 21:39:15 -0400

    accordian working - did not add data yet

--- app/controllers/bootstrap.js
+++ app/controllers/bootstrap.js
@@ -1,1 +1,14 @@
-// angular.module('myApp',['ngAnimate', 'ui.bootstrap']);
+myApp.controller('AccordionDemoCtrl', function ($scope) {
+
+  $scope.groups = [
+    {
+      title: 'Select Degree Program and Course',
+      content: 'Dynamic Group Body - 1'
+    },
+    {
+      title: 'Add Rubric',
+      content: 'Dynamic Group Body - 2'
+    }
+  ];
+
+});
