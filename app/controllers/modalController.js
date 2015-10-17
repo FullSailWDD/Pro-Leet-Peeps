71c8907b9548e71ebb41a62cb93ea92f3aa8d0da
Jeffrey Mendez <jeffmendez19@gmail.com>
2015-10-16 21:08:46 -0400

    adding download string function

--- app/controllers/modalController.js
+++ app/controllers/modalController.js
@@ -8,11 +8,17 @@ myApp.controller('ModalInstanceCtrl', function ($scope, $modalInstance, data, $h
 
 $scope.update = function() {
   console.log($scope.data._id);
+    var a = document.body.appendChild(
+        document.createElement("a")
+    );
+    a.download = "export.html";
+    a.href = "data:text/html," + document.getElementById("test").innerHTML; // Grab the HTML
+    a.click(); // Trigger a click on the element
+    // Change the string in the result using JSON.parse from string
+
   $http.put('/api/courses/' + $scope.data._id, $scope.data).success(function(response) {
 
   $modalInstance.close();
-
-
 })
 };
 })
