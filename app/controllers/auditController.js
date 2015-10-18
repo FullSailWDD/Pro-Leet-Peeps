7fb5102a5e65f2595fdb4b1b9c7e0e04ae45382d
alexander garza <alexzangarza@gmail.com>
2015-10-18 18:36:31 -0400

    started audit page

--- app/controllers/auditController.js
+++ app/controllers/auditController.js
@@ -12,9 +12,11 @@ refresh();
   $scope.selected = {
     data: $scope.data
   };
-    console.log("Scopre.data modalInstance",$scope.data);
+
+    
+    console.log("Scope.data modalInstance",$scope.data);
 $scope.update = function() {
-  console.log("======", $scope.data);
+  console.log("======", $scope.data);    
   $http.put('/api/courses/' + $scope.data._id, $scope.data).success(function(response) {
   $modalInstance.close();
 })
@@ -29,5 +31,19 @@ $scope.actionLeave = function(){
   $location.path('/myCourse')
   $modalInstance.close();
 }
-
+    //grabbing the grades and sections
+    var grades = $scope.data.rubric.grade;
+    var sections = $scope.data.rubric.title;
+    //split the string from the dataBase
+    var gradeSplit = grades.split(",");
+    var sectionSplit = sections.split(",");
+    //making it viewable to scope
+    $scope.gradeSplit = gradeSplit;
+    $scope.sectionSplit = sectionSplit;
+    
+    
+    console.log("grade Split" , gradeSplit);
+    console.log("Section Split" , sectionSplit);
 })
+
+
